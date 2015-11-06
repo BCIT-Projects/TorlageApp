6eabb29652c374ba0dd1846ab30efa026e1ad389
Nadia Dobrianskaia <nadezhdadobrianskaia@gmail.com>
2015-11-05 19:55:32 -0800

    backend working for selectPerformers for the director page

--- TorlageProjectApp/SelectPerformers.aspx.cs
+++ TorlageProjectApp/SelectPerformers.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -40,26 +43,111 @@ namespace TorlageProjectApp
         protected void Calendar1_SelectionChanged(object sender, EventArgs e)
         {
             TextBoxShowDate.Text = CalendarShowDate.SelectedDate.ToString();
+            TextBoxSetShowDate.Text = CalendarShowDate.SelectedDate.ToString();
         }
 
-        protected void ListView2_SelectedIndexChanged(object sender, EventArgs e)
+
+        /// <summary>
+        /// A way to select performers and add them to the Add Performer List.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void ButtonSelectPeople_Click(object sender, EventArgs e)
         {
-            
-            
-            
+            TextBoxAddPerformers.Text = "";
+            foreach (GridViewRow row in GridView1.Rows)
+            {
+                CheckBox checkbox = (CheckBox)row.FindControl("CheckBoxSelectPerformer");
+                if (checkbox.Checked)
+                {
+                    // Retreive the Performer Name
+                    string Performer = (String)GridView1.DataKeys[row.RowIndex].Value;
+                    //int PerformerName = Convert.ToInt32(GridView1.DataKeys[row.RowIndex].Value);
+                    TextBoxAddPerformers.Text += Performer + ", " + "\r\n";
+                }
+            }
+
         }
 
-        protected void CheckBoxPerformerSelected_CheckedChanged(object sender, EventArgs e)
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void ButtonSetShow_Click(object sender, EventArgs e)
         {
-            ViewState["CheckBox"] = true;
-            if (ViewState["CheckBox"] == null)
+
+
+            //---------------Pull out all the performer Names Note might need to change the Name to id
+            ArrayList users = new ArrayList();
+            ArrayList usersFilled = new ArrayList();
+            SqlConnection connection = new SqlConnection();   //establish an connection to the SQL server 
+            connection.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ToConnectionString"].ConnectionString;
+            string selectCommand = "SELECT * FROM Performers WHERE Performers.Active = 1 AND NOT Exists(select PerformerID from PerformersAvailable where ScheduleDate ='" + TextBoxSetShowDate.Text + "')";
+           
+            SqlCommand command = new SqlCommand(selectCommand, connection);
+           
+            connection.Open();
+            SqlDataReader reader = null;
+            try
             {
-                ;
+                reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    users.Add((int)reader["PerformerId"]);
+                    //string value = (string)reader["PerformerName"];
+                    //Label1.Text += value;
+                }
+          
             }
-            else
+            catch (Exception ex)
             {
-                TextBoxAddPerformers.Text = "Changed Text";
+                reader.Close();
+                connection.Close();
+                Label1.Text = "caught Exception";
             }
+
+            foreach (int entry in users)
+            {
+                Label1.Text += entry + ", ";
+            }
+
+
+            //----------------end of how to pull out the performers' names (or id for future)
+
+            //a way to add a row
+            
+            SqlConnection cnn = new SqlConnection();
+            cnn.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ToConnectionString"].ConnectionString;
+            cnn.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "SELECT * From PerformersAvailable Where ScheduleDate = '" + TextBoxSetShowDate.Text + "'";
+            cmd.Connection = cnn;
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = cmd;
+            DataSet ds = new DataSet();
+            da.Fill(ds, "PerformersAvailable");
+            SqlCommandBuilder cb = new SqlCommandBuilder(da);
+
+            foreach (int entry in users)
+            {
+                
+                    DataRow drow = ds.Tables["PerformersAvailable"].NewRow();
+                    drow["ScheduleDate"] = TextBoxSetShowDate.Text;
+                    drow["PerformerID"] = entry;
+                    drow["Available"] = "1";
+                    drow["TentativeShow"] = "1";
+                    ds.Tables["PerformersAvailable"].Rows.Add(drow);
+                    da.Update(ds, "PerformersAvailable");
+                
+            }
+            cnn.Close();
+             
         }
+
+
+
+
     }
 }
